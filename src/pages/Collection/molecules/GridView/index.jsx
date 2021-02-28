0419e4004b3695db210f5436a7ede2bac3439c4f
Ritesh <riteshch2545@gmail.com>
2021-02-28 22:20:25 +0530

    Added minor sturtcural changes

--- src/pages/Collection/molecules/GridView/index.jsx
+++ src/pages/Collection/molecules/GridView/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import { PropTypes } from 'prop-types'
 
-import BookCard from '../../components/BookCard'
+import BookCard from '../../../../components/BookCard'
 import useStyles from './style'
 
 const GridView = ({ result, handleOpen }) => {
--- src/pages/Collection/molecules/ListView/index.jsx
+++ src/pages/Collection/molecules/ListView/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 
-import Table from '../../components/Table'
+import Table from '../../../../components/Table'
 
 const ListView = ({ result }) => {
   return (
